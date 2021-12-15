83d95910e9df8ea07f8ed7d663bad431d6865410
Sasha Sokolov <sashasokolov777@gmail.com>
2021-12-15 23:30:02 +0200

    jwt refactor v4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"Calendar/controller"
 	"Calendar/entity"
 	database "Calendar/initdb.d"
 	router "Calendar/internal/server/http"
-	"fmt"
 	"log"
-	"net/http"
-)
-
-var (
-	eventController controller.EventController = controller.NewEventController()
-	authController  controller.AuthController  = controller.NewAuthController()
-	httpRouter      router.Router              = router.NewMuxRouter()
 )
 
 func main() {
-	const port string = ":8000"
-
 	err := database.InitDatabase()
 	if err != nil {
 		log.Fatalln("could not create database", err)
@@ -29,21 +18,5 @@ func main() {
 		log.Fatalln("could not migrate user model", err)
 	}
 
-	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintln(w, "Up and running...")
-		if err != nil {
-			log.Fatalln("Error!")
-		}
-	})
-
-	httpRouter.GET("/events", eventController.GetAll)
-	httpRouter.GET("/event", eventController.GetOne)
-	httpRouter.POST("/event", eventController.Add)
-	httpRouter.PUT("/event", eventController.Update)
-	httpRouter.DELETE("/event", eventController.Delete)
-
-	httpRouter.POST("/signup", authController.Signup)
-	httpRouter.POST("/login", authController.Login)
-
-	httpRouter.SERVE(port)
+	router.Init()
 }
--- internal/server/http/router.go
+++ internal/server/http/router.go
@@ -1,6 +1,13 @@
 package http
 
-import "net/http"
+import (
+	"Calendar/controller"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":8000"
 
 type Router interface {
 	GET(uri string, f func(w http.ResponseWriter, r *http.Request))
@@ -8,4 +15,29 @@ type Router interface {
 	PUT(uri string, f func(w http.ResponseWriter, r *http.Request))
 	DELETE(uri string, f func(w http.ResponseWriter, r *http.Request))
 	SERVE(port string)
+}
+
+var (
+	eventController controller.EventController = controller.NewEventController()
+	authController  controller.AuthController  = controller.NewAuthController()
+	httpRouter      Router                     = NewMuxRouter()
+)
+
+func Init() {
+	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintln(w, "Up and running...")
+		if err != nil {
+			log.Fatalln("Error!")
+		}
+	})
+	httpRouter.GET("/events", eventController.GetAll)
+	httpRouter.GET("/event", eventController.GetOne)
+	httpRouter.POST("/event", eventController.Add)
+	httpRouter.PUT("/event", eventController.Update)
+	httpRouter.DELETE("/event", eventController.Delete)
+
+	httpRouter.POST("/signup", authController.Signup)
+	httpRouter.POST("/login", authController.Login)
+
+	httpRouter.SERVE(port)
 }
